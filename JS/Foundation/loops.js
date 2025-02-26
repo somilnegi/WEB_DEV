1d38a7e073c07c2452de8dcbde73025c4a08ac76
somilnegi <somilnegi12345@gmail.com>
2025-02-26 17:04:05 +0530

    Adding loops (more data)

--- JS/Foundation/loops.js
+++ JS/Foundation/loops.js
@@ -13,7 +13,6 @@
 //     countdown.push(i);
 //     i--;
 // }
-
 // console.log(countdown);
 
 
@@ -25,7 +24,6 @@
 //         teaCollection.push(tea);
 //     }
 // } while (str !== "stop");
-
 // console.log(teaCollection);
 
 
@@ -35,7 +33,6 @@
 //     total += k;
 //     k++;
 // } while (k <= 3);
-
 // console.log(total);
 
 
@@ -44,15 +41,72 @@
 // for (let i = 0; i < array.length; i++) {
 //     multipliedNumber.push(array[i]*2)
 // }
-
 // console.log(multipliedNumber);
 
 
-let array = ["Paris", "New York", "Tokya", "London"]
-let cityList = []
+// let array = ["Paris", "New York", "Tokya", "London"]
+// let cityList = []
+// for (let i = 0; i < array.length; i++) {
+//     cityList.push(array[i])
+// }
+// console.log(cityList);
 
-for (let i = 0; i < array.length; i++) {
-    cityList.push(array[i])
+
+// let array = ["green tea", "black tea", "chai", "oolong tea"]
+// let selectedTeas=[]
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] === "chai") {
+//         break;
+//     }
+//     selectedTeas.push(array[i]);
+// }
+// console.log(selectedTeas);
+
+
+// let cities=["london", "new york", "paris", "berlin"]
+// let visistedCities=[]
+// for (let i = 0; i < cities.length; i++) {
+//     if (cities[i] == "paris") {
+//         continue
+//     }
+//     visistedCities.push(cities[i])
+// }
+// console.log(visistedCities);
+
+
+// let numbers=[1,2,3,4,5]
+// let smallNumbers=[]
+// for (const i of numbers) {
+//     if (i == 4) {
+//         break;
+//     }
+//     smallNumbers.push(i);
+// }
+// console.log(smallNumbers);
+
+
+// let teas = ["chai", "green tea", "herbal tea", "black tea"]
+// let preferredTeas=[]
+// for (const tea of teas) {
+//     if (tea == "herbal tea") {
+//         continue
+//     }
+//     preferredTeas.push(tea)
+// }
+// console.log(preferredTeas);
+
+let cityPopulation = {
+    london: 8900000, 
+    "new york": 8400000,
+    paris: 2200000,
+    berlin: 3500000,
 }
+let cityPopulations = {}
 
-console.log(cityList);
+for (const city in cityPopulation) {
+    if (city == "berlin") {
+        break
+    }
+    cityPopulations[city]=cityPopulation[city]
+}
+console.log(cityPopulations);
