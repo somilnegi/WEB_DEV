318c1ff9072be98d95d7ad0ada422bc18514348c
somilnegi <somilnegi12345@gmail.com>
2025-02-26 16:28:38 +0530

    Adding loops

---
+++ JS/Foundation/loops.js
@@ -0,0 +1,58 @@
+// let sum=0;
+// let i = 1;
+// while (i <= 5) {
+//     sum += i;
+//     i++;
+// }
+// console.log(sum);
+
+
+// let i = 5;
+// let countdown=[]
+// while (i > 0) {
+//     countdown.push(i);
+//     i--;
+// }
+
+// console.log(countdown);
+
+
+// let teaCollection = []
+// let tea;
+// do {
+//     tea=prompt("Enter you favourite tea , type stop to finish")
+//     if (tea!=="stop") {
+//         teaCollection.push(tea);
+//     }
+// } while (str !== "stop");
+
+// console.log(teaCollection);
+
+
+// let total = 0;
+// let k = 1;
+// do {
+//     total += k;
+//     k++;
+// } while (k <= 3);
+
+// console.log(total);
+
+
+// let array=[2,4,6]
+// let multipliedNumber=[]
+// for (let i = 0; i < array.length; i++) {
+//     multipliedNumber.push(array[i]*2)
+// }
+
+// console.log(multipliedNumber);
+
+
+let array = ["Paris", "New York", "Tokya", "London"]
+let cityList = []
+
+for (let i = 0; i < array.length; i++) {
+    cityList.push(array[i])
+}
+
+console.log(cityList);
