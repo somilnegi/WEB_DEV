dac983454c3be2385c5be795b6356bbfbc283ded
Somil Negi <somilnegi12345@gmail.com>
2025-04-05 15:27:16 +0530

    Adding routes and controllers files

--- BACKEND/Vidtube/src/app.js
+++ BACKEND/Vidtube/src/app.js
@@ -14,4 +14,9 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 
+// Import routes
+import healthCheckRouter from "./routes/healthCheck.routes.js"
+
+// Routes
+app.use("/api/v1/healthcheck", healthCheckRouter)
 export {app}
