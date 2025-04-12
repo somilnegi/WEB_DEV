abe9a817707d354fa0b64b5534c9ec949d34d7f4
Somil Negi <somilnegi12345@gmail.com>
2025-04-12 17:02:33 +0530

    Adding multer and cloudinary

--- BACKEND/Vidtube/src/routes/healthCheck.routes.js
+++ BACKEND/Vidtube/src/routes/healthCheck.routes.js
@@ -4,6 +4,7 @@ import { healthCheck } from "../controllers/healthCheck.controllers.js";
 const router = Router()
 
 router.route("/").get(healthCheck)
+
 router.route("/test").get(healthCheck)
 
 export default router
