b50b3f784c4503542b92425e7c3b99336f1ac106
Somil Negi <somilnegi12345@gmail.com>
2025-05-15 04:23:16 +0530

    updated code

--- BACKEND/Vidtube/src/routes/healthCheck.routes.js
+++ BACKEND/Vidtube/src/routes/healthCheck.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { healthCheck } from "../controllers/healthCheck.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router()
-
+router.use(verifyJWT)
 router.route("/").get(healthCheck)
 
 // router.route("/test").get(healthCheck)
--- BACKEND/Vidtube/src/routes/user.routes.js
+++ BACKEND/Vidtube/src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middlewares.js"
-import { registerUser, logoutUser } from "../controllers/user.controller.js";
+import { registerUser, logoutUser, loginUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, getUserChannelProfile, updateAccountDetails, updateUserCoverImage, updateUserAvatar, getWatchHistory } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router()
@@ -19,7 +19,17 @@ router.route("/register").post(
     registerUser,
 )
 
+router.route("/login").post(loginUser)
 // Secured routes
+
 router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
