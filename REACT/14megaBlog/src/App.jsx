64ea984fd2509bea0a1835f299ba417072aea43e
Somil Negi <somilnegi12345@gmail.com>
2025-09-13 13:44:20 +0530

    add new project: 14megaBlog

---
+++ REACT/14megaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import conf from './config/config';
+
+function App() {
+
+  console.log(conf.appwriteUrl);
+  
+
+  return (
+    <>
+      <h1>A blog app with Appwrite</h1>
+    </>
+  )
+}
+
+export default App
